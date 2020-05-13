2d565fc27d0947a4afd2c0efb7a6107b911924e7
Avaer Kazmer <a@modules.io>
2020-05-13 12:56:18 -0400

    Add gltf model url redirect to proxy

--- src/components/gltf-model-plus.js
+++ src/components/gltf-model-plus.js
@@ -7,6 +7,7 @@ import { getCustomGLTFParserURLResolver } from "../utils/media-url-utils";
 import { promisifyWorker } from "../utils/promisify-worker.js";
 import { MeshBVH, acceleratedRaycast } from "three-mesh-bvh";
 import { disposeNode, cloneObject3D } from "../utils/three-utils";
+import {proxyHost} from '../utils/phoenix-utils';
 
 THREE.Mesh.prototype.raycast = acceleratedRaycast;
 
@@ -416,6 +417,13 @@ export async function loadGLTF(src, contentType, preferredTechnique, onProgress,
 }
 
 export async function loadModel(src, contentType = null, useCache = false, jsonPreprocessor = null) {
+  if (/^https:\/\/dev\.reticulum\.io\//.test(src)) {
+    src = `https://${proxyHost}/` + encodeURIComponent(src);
+  }
+  if (/^https:\/\/cors-proxy-dev\.reticulum\.io\//.test(src)) {
+    src = `https://${proxyHost}/` + encodeURIComponent(src.replace(/^https:\/\/cors-proxy-dev\.reticulum\.io\//, ''));
+  }
+
   const preferredTechnique =
     window.APP && window.APP.quality === "low" ? "KHR_materials_unlit" : "pbrMetallicRoughness";
 
