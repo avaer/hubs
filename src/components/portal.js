6f70d340d73e89230f4caa34c3dd23078a749965
Avaer Kazmer <a@modules.io>
2020-05-24 23:26:43 -0400

    More portal work

--- src/components/portal.js
+++ src/components/portal.js
@@ -1,5 +1,14 @@
 const { Vector3, Quaternion, Matrix4, Euler } = THREE;
 
+const localVector = new Vector3();
+const localVector2 = new Vector3();
+const localVector3 = new Vector3();
+const localVector4 = new Vector3();
+const localQuaternion = new Quaternion();
+const localQuaternion2 = new Quaternion();
+const localMatrix = new Matrix4();
+const localMatrix2 = new Matrix4();
+
 const portalSides = {
   left: null,
   right: null,
@@ -27,19 +36,49 @@ AFRAME.registerComponent('portal', {
     } */
   },
   tick(time, dt) {
-    const position = this.el.getAttribute('position');
     const side = this.el.getAttribute('side');
-    const models = Array.from(document.querySelectorAll('[medialoader][networked]'));
-    const _getDistance = m => m.el.object3D.position.distanceTo(position);
-    const closestModels = models.sort((a, b) => _getDistance(a) - _getDistance(b));
-    console.log('tick', this, closestModels, portalSides);
-    if (closestModels.length > 0) {
-      const closestModel = closestModels[0];
-      portalSides[side] = closestModel;
+    const viewingRig = document.getElementById('viewing-rig');
+    if (side && !portalSides[side] && viewingRig) {
+      localMatrix
+        .compose(this.el.getAttribute('position') || localVector.set(0, 0, 0), this.el.getAttribute('quaternion') || localQuaternion.set(0, 0, 0, 1), localVector2.set(1, 1, 1))
+        .premultiply(localMatrix2.getInverse(viewingRig.object3D.matrix))
+        .premultiply(window.xrpackage.package.matrix)
+        .decompose(localVector, localQuaternion, localVector2);
+      
+      if (side === 'left') {
+        const medias = Array.from(document.querySelectorAll('[medialoader][networked]'));
+        const _getDistance = m => m.el.object3D.position.distanceTo(position);
+        const closestModels = medias.sort((a, b) => _getDistance(a) - _getDistance(b));
+        if (closestModels.length > 0) {
+          const closestModel = closestModels[0];
+          if (_getDistance(closestPackage) < 0.3) {
+            closestModel.setAttribute('position', this.getAttribute('position'));
+            closestModel.setAttribute('quaternion', this.getAttribute('quaternion'));
+            closestModel.setAttribute('scale', this.getAttribute('scale'));
+            portalSides[side] = closestModel;
+          }
+        }
+      } else if (side === 'right') {
+        const packages = window.xrpackage.packages.filter(p => p !== window.xrpackage.package);
+        const _getDistance = p => {
+          p.matrix.decompose(localVector3, localQuaternion2, localVector4);
+          return localVector3.distanceTo(localVector);
+        };
+        const closestPackages = packages.sort((a, b) => _getDistance(a) - _getDistance(b));
+        if (closestPackages.length > 0) {
+          const closestPackage = closestPackages[0];
+          closestPackage.matrix.decompose(localVector3, localQuaternion2, localVector4);
+          if (_getDistance(closestPackage) < 1) {
+            closestPackage.setMatrix(localMatrix);
+            closestPackage.grabrelease();
+            portalSides[side] = closestPackage;
+          }
+        }
+      }
+
+      const portalButton = document.querySelector('[portal-button]');
+      portalButton.setAttribute('visible', Object.keys(portalSides).some(k => portalSides[k]));
     }
-    /* if (!this.ikRoot) {
-      return;
-    } */
   },
 });
 AFRAME.registerComponent('portal-button', {
